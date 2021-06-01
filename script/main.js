7d4179e934a96a293f928bc346e29edfb7f8cc86
Addis Belete <addis.bel@gmail.com>
2021-06-01 12:50:26 +0300

    Add DOM

---
+++ script/main.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book() {
+const author = document.getElementsByClassName('author')
+const title = document.getElementsByClassName('title')
+const numOfPage = document.getElementsByClassName('number')
+
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
