d279483ffd6ed988dd281b100035c2ee9f2eff00
Addis Belete <addis.bel@gmail.com>
2021-06-02 11:00:36 +0300

    Add a function to add book

--- script/main.js
+++ script/main.js
@@ -1,12 +1,32 @@
 let myLibrary = [];
+const author = document.querySelector('.author')
+const title = document.querySelector('.title')
+const bookPage = document.querySelector('.number')
+const read = document.querySelector('#dropdown')
+const myHeading = document.querySelector('h1')
+myHeading.textContent = "is it working"
 
-function Book() {
-const author = document.getElementsByClassName('author')
-const title = document.getElementsByClassName('title')
-const numOfPage = document.getElementsByClassName('number')
-
+function Book(author, title, numOfPage, read) {
+	this.author = author;
+	this.title = title;
+	this.numOfPage = numOfPage;
+	this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+	if (author.value && title.value && bookPage.value && read.value) {
+		myLibrary.push(new Book(author.value, title.value, bookPage.value, read.value))
+		alert("Book added successfully")
+
+	} else {
+		alert("please enter all information")
+	}
+}
+
+submit.addEventListener('click', (e) => {
+	addBookToLibrary()
+})
+
+function displayBooks() {
+
 }
