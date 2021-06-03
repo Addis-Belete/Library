bfcf6bc4fc4dea3089debdde49415c02a030c8fe
Addis Belete <addis.bel@gmail.com>
2021-06-03 16:31:18 +0300

    Add style to form and table

--- script/main.js
+++ script/main.js
@@ -1,115 +1,118 @@
-window.onload = function() {
-  myform = document.getElementById('myform')
+window.onload = function () {
+	myform = document.getElementById('myform')
 
-  dynamicHere= document.getElementById('dynamicHere')
-  cardBody = document.getElementsByClassName('cart-body')[0]
+	dynamicHere = document.getElementById('dynamicHere')
+	cardBody = document.getElementsByClassName('cart-body')[0]
 
-  myform.addEventListener('submit',function(e) {
-    e.preventDefault();
-    authorName = document.getElementById('authorName').value
-    bookName = document.getElementById('bookName').value
-    isbn = document.getElementById('isbn').value
+	myform.addEventListener('submit', function (e) {
+		e.preventDefault();
+		authorName = document.getElementById('authorName').value
+		bookName = document.getElementById('bookName').value
+		isbn = document.getElementById('isbn').value
+		bookRead = document.getElementById('dropdown').value
 
-    if(authorName == '' || bookName == '' || isbn == ''){
-      UI.messages('Enter All fields', 'Danger');
-      return
-    } else {
-      var book = new Book(authorName, bookName, isbn)
-          UI.displayData(book)
-          Store.setStored(book)
-          UI.clearfields()
-          UI.messages('Data inserted', 'success')
-    }
-  })
+		if (authorName == '' || bookName == '' || isbn == '' || bookRead == '') {
+			UI.messages('Enter All fields', 'Danger');
+			return
+		} else {
+			var book = new Book(authorName, bookName, isbn, bookRead)
+			UI.displayData(book)
+			Store.setStored(book)
+			UI.clearfields()
+			UI.messages('Data inserted', 'success')
+		}
+	})
 
-  dynamicHere.addEventListener('click', function(e){
-     UI.removeRow(e.target)
-  })
+	dynamicHere.addEventListener('click', function (e) {
+		UI.removeRow(e.target)
+	})
 
-  class Book {
-    constructor(authorName, bookName, isbn){
-    this.authorName = authorName
-    this.bookName = bookName
-    this.isbn = isbn
-    }
-  }
+	class Book {
+		constructor(authorName, bookName, isbn, read) {
+			this.authorName = authorName
+			this.bookName = bookName
+			this.isbn = isbn
+			this.read = read
+		}
+	}
 
-  class UI {
-    static clearfields(){
-      document.getElementById('authorName').value = ''
-      document.getElementById('bookName').value = ''
-      document.getElementById('isbn').value = ''
-    }
+	class UI {
+		static clearfields() {
+			document.getElementById('authorName').value = ''
+			document.getElementById('bookName').value = ''
+			document.getElementById('isbn').value = ''
+		}
 
-    static displayData(book) {
-     let books = Store.getstored()
-     books.push(book)
-      UI.PopulateRow(books)
-    }
+		static displayData(book) {
+			let books = Store.getstored()
+			books.push(book)
+			UI.PopulateRow(books)
+		}
 
-    static PopulateRow(books){
-      while(dynamicHere.firstChild){
-        dynamicHere.firstChild.remove(dynamicHere.firstChild)
-      }
-      books.forEach(everydata => {
-           dynamicHere.innerHTML += `
+		static PopulateRow(books) {
+			while (dynamicHere.firstChild) {
+				dynamicHere.firstChild.remove(dynamicHere.firstChild)
+			}
+			books.forEach(everydata => {
+				dynamicHere.innerHTML += `
         <tr>
-              <td>${everydata.isbn}</td>
               <td>${everydata.authorName}</td>
               <td>${everydata.bookName}</td>
-              <td><button class='btn btn-danger removeit'>Close</button></td>
+              <td>${everydata.isbn}</td>
+<td>${everydata.read}</td>
+              <td><button class='btn btn-danger removeit'>X</button></td>
         </tr>`
-      })
-     
-    }
+			})
 
-    static messages(txt, className){
-      let div = document.createElement('div')
-      div.className = `alert alert - ${className}`
-      div.innerHTML = txt;
-      cardBody.insertBefore(div, myform)
+		}
 
-      setTimeout(function(){
-        div.remove()
-      }, 2000)
-    }
+		static messages(txt, className) {
+			let div = document.createElement('div')
+			div.className = `alert alert - ${className}`
+			div.innerHTML = txt;
+			cardBody.insertBefore(div, myform)
 
-    static removeRow(element){
-      if (element.classList.contains('removeit')){
-         let isbn = element.parentElement.parentElement.firstElementChild.innerText;
-        Store.removeStoredValue(isbn)
-        element.parentElement.parentElement.remove();
-      }
-    }
-  }
+			setTimeout(function () {
+				div.remove()
+			}, 2000)
+		}
 
-  class Store {
-     static getstored(){
-       let books = ''
-       if (localStorage.getItem('book') == null){
-         books = []
-       } else {
-         books = JSON.parse(localStorage.getItem('book'))
-       }
-       return books
-     }
+		static removeRow(element) {
+			if (element.classList.contains('removeit')) {
+				let isbn = element.parentElement.parentElement.firstElementChild.innerText;
+				Store.removeStoredValue(isbn)
+				element.parentElement.parentElement.remove();
+			}
+		}
+	}
 
-     static setStored(obj){
-       let booksFromLocal = Store.getstored()
-       booksFromLocal.push(obj)
-       localStorage.setItem('book', JSON.stringify(booksFromLocal))
-     }
+	class Store {
+		static getstored() {
+			let books = ''
+			if (localStorage.getItem('book') == null) {
+				books = []
+			} else {
+				books = JSON.parse(localStorage.getItem('book'))
+			}
+			return books
+		}
 
-     static removeStoredValue(isbn){
-       let Albooks = Store.getstored()
-       Albooks.forEach(function(everydata, index){
-         if(everydata.isbn == isbn){
-           Albooks.splice(index, 1)
-         }
-       })
-       localStorage.setItem('book', JSON.stringify(Albooks))
-     }
-  }
+		static setStored(obj) {
+			let booksFromLocal = Store.getstored()
+			booksFromLocal.push(obj)
+			localStorage.setItem('book', JSON.stringify(booksFromLocal))
+		}
 
-  UI.PopulateRow(Store.getstored())
+		static removeStoredValue(isbn) {
+			let Albooks = Store.getstored()
+			Albooks.forEach(function (everydata, index) {
+				if (everydata.isbn == isbn) {
+					Albooks.splice(index, 1)
+				}
+			})
+			localStorage.setItem('book', JSON.stringify(Albooks))
+		}
+	}
+
+	UI.PopulateRow(Store.getstored())
 }
